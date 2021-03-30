77523c98100f56457d63446f5c9e61554d5a91a8
Gerardo Lopez <59430166+GerardoLG@users.noreply.github.com>
2021-03-29 23:59:13 -0600

    Refactor(helpers): add more returning info getWeather.js

--- src/helpers/getWeather.js
+++ src/helpers/getWeather.js
@@ -3,12 +3,15 @@ export const getWeather = async (city) => {
     city
   )}&appid=5be534d75afe350256ad6e98433c520a&units=metric`;
   const res = await fetch(url);
-  const { main, weather } = await res.json();
+  const data = await res.json();
+  const { main, weather, wind } = data;
 
   const currentWeather = {
-    temp: main.temp,
     feels_like: main.feels_like,
+    temp: main.temp,
     description: weather[0].description,
+    icon: weather[0].icon,
+    speed: wind.speed,
   };
 
   return currentWeather;
